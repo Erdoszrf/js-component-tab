6708f75d9e85571aad3667f4c4069b61264a9a67
erdoszrf <erdoszrf@qq.com>
2018-02-02 22:48:14 +0800

    first

---
+++ tab.js
@@ -0,0 +1,24 @@
+function Tab (node){
+  Tab.prototype.init()
+}
+
+Tab.prototype = {
+    constructor: Tab,
+    init: function(){
+    this.bind()
+    },
+    bind: function(){
+      document.querySelectorAll('.tab-container')[0].onclick = function(){
+        document.querySelector('.div1').classList.add('active')
+        document.querySelector('.div2').classList.remove('active')
+      }
+      document.querySelectorAll('.tab-container')[1].onclick = function(){
+        document.querySelector('.div1').classList.remove('active')
+        document.querySelector('.div2').classList.add('active')
+      }
+    },
+
+}
+
+var tab1 = new Tab(document.querySelectorAll('.tab-container')[0]);
+var tab2 = new Tab(document.querySelectorAll('.tab-container')[1]);
