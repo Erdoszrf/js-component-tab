230c2ebdf2fccab92a12d2b48f37237245dee510
erdoszrf <erdoszrf@qq.com>
2018-02-03 10:23:26 +0800

    一丢丢优化

--- tab.js
+++ tab.js
@@ -9,10 +9,14 @@ Tab.prototype = {
     },
     bind: function(){
       document.querySelectorAll('.tab-container')[0].onclick = function(){
+        document.querySelectorAll('.tab-container')[0].classList.add('active')
+        document.querySelectorAll('.tab-container')[1].classList.remove('active')
         document.querySelector('.div1').classList.add('active')
         document.querySelector('.div2').classList.remove('active')
       }
       document.querySelectorAll('.tab-container')[1].onclick = function(){
+        document.querySelectorAll('.tab-container')[1].classList.add('active')
+        document.querySelectorAll('.tab-container')[0].classList.remove('active')
         document.querySelector('.div1').classList.remove('active')
         document.querySelector('.div2').classList.add('active')
       }
@@ -22,3 +26,6 @@ Tab.prototype = {
 
 var tab1 = new Tab(document.querySelectorAll('.tab-container')[0]);
 var tab2 = new Tab(document.querySelectorAll('.tab-container')[1]);
+
+
+
